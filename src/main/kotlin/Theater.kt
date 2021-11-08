480f219973ca09b6ea406372e7c86a9251e187d6
최영서 <lukas@MacBook-Pro.local>
2021-11-09 00:07:14 +0900

    의존성 제거

--- src/main/kotlin/Theater.kt
+++ src/main/kotlin/Theater.kt
@@ -1,13 +1,7 @@
 class Theater(private val ticketSeller: TicketSeller) {
+
     fun enter(audience: Audience) {
-        if (audience.bag.hasInvitation()) {
-            val ticket = ticketSeller.ticketOffice.getTicket()
-            audience.bag.ticket = ticket
-        } else {
-            val ticket = ticketSeller.ticketOffice.getTicket()
-            audience.bag.minusAmount(ticket.fee)
-            ticketSeller.ticketOffice.plusAmount(ticket.fee)
-            audience.bag.ticket = ticket
-        }
+        ticketSeller.sellTo(audience)
     }
+
 }
